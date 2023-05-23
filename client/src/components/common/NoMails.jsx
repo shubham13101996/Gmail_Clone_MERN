9e406f9b0ab23d77efd63523fd475275ea039430
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-05-23 14:50:43 +0530

    set the messages for all the type when no mail is there

--- client/src/components/common/NoMails.jsx
+++ client/src/components/common/NoMails.jsx
@@ -1,12 +1,26 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography, styled } from "@mui/material";
 import React from "react";
 
 const NoMails = ({ message }) => {
+  const Component = styled(Box)({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 50,
+    opacity: 0.8,
+    width: "100%",
+  });
+
+  const DividerWrapper = styled(Divider)({
+    width: "100%",
+    marginTop: 10,
+  });
   return (
-    <Box>
+    <Component>
       <Typography>{message.heading}</Typography>
-      <Typography>{message.heading}</Typography>
-    </Box>
+      <Typography>{message.subHeading}</Typography>
+      <DividerWrapper />
+    </Component>
   );
 };
 
